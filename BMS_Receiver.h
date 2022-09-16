8a07f5e080cadeb75fb1267964e1937e4ebfaa77
YashParsai <105203897+YashParsai@users.noreply.github.com>
2022-09-16 14:19:16 +0530

    Create BMS_Receiver.h

---
+++ BMS_Receiver.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void recieveDataFromConsole(char *recv_buff);
+void extractTempData(int* temp_buff, char* recv_buff);
+void extractSoCData(int* soc_buff, char* recv_buff);
+int getMaxTemp(int*);
+int getMinTemp(int*);
+int getMaxSoC(int*);
+int getMinSoC(int*);
+void printSoCData(int* soc_buff);
+void printTempData(int* temp_buff);
